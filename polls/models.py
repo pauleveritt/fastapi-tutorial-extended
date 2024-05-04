206dfe9ae3b9bd139fd8da760b9ba359162fcaef
Mukul Mantosh <mukul.mantosh@jetbrains.com>
2024-05-04 21:13:11 +0530

    - Fixed Foreign Key mapping

--- polls/models.py
+++ polls/models.py
@@ -7,9 +7,19 @@ class Question(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     question_text: str
     pub_date: datetime
-    # TODO When this is uncommented, fails for URL /questions/
-    # choices: list["Choice"] = Relationship(back_populates="question")
+    choices: list["Choice"] = Relationship(back_populates="question")
 
     def was_published_recently(self) -> bool:
         now = datetime.now()
         return now - timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    choice_text: str
+    votes: int = Field(default=0)
+    question_id: int = Field(foreign_key="question.id")
+    question: Question | None = Relationship(back_populates="choices")
+
+
+
