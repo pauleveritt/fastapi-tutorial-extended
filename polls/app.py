17275bac4da111dc676cd74d80b42af4fd70687e
pauleveritt <paul.everitt@jetbrains.com>
2024-05-05 08:01:15 -0400

    Fix CORS.

--- polls/app.py
+++ polls/app.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import event
 from sqlmodel import SQLModel
 
@@ -23,6 +24,15 @@ for table in SQLModel.metadata.tables.values():
     event.listen(table, 'after_create', initialize_table)
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:5173/",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 package_dir = Path(__file__).parent.parent.absolute()
 static_dir = package_dir / "static"
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
--- src/frontend/Index.jsx
+++ src/frontend/Index.jsx
@@ -1,33 +1,37 @@
 import React, { useEffect, useState } from "react";
 
 export function Index() {
-  const URL = "http://localhost:8000/v1/question/";
-  let [questions, setQuestions] = useState(null);
+    const URL = "http://localhost:8000/v1/question/";
+    let [questions, setQuestions] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(URL)
-        .then((response) => response.json())
-        .then((data) => {
-          setQuestions(data);
-        });
-    };
-    // noinspection JSIgnoredPromiseFromCall
-    fetchData();
-  }, []);
-  return (
-    <div className="grid gap-4 grid-cols-4 mx-2 mt-2">
-      <h1>Questions</h1>
-      <ul>
-        {questions &&
-          questions.map((question) => (
-            <li key={question.id}>
-              <a href={`${URL}/${question.id}`} aria-label="Question">
-                {question.question_text}
-              </a>
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(URL)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(999, data);
+                    setQuestions(data);
+                });
+        };
+        // noinspection JSIgnoredPromiseFromCall
+        fetchData();
+    }, []);
+    return (
+        <div className="grid gap-4 grid-cols-4 mx-2 mt-2">
+            <h1>Questions</h1>
+            <ul>
+                {questions &&
+                    questions.map((question) => (
+                        <li key={question.id}>
+                            <a
+                                href={`${URL}/${question.id}`}
+                                aria-label="Question"
+                            >
+                                {question.question_text}
+                            </a>
+                        </li>
+                    ))}
+            </ul>
+        </div>
+    );
 }
